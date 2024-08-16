e6bd8459c4a59db9b335454990a3fb7b00f66c13
witvick <nitishkumar37373@gmail.com>
2024-08-16 08:42:25 +0530

    Test_Class ,test_methods

---
+++ Test_Opencart/test_cart1.py
@@ -0,0 +1,11 @@
+#"C:\Users\MR.WICK\PycharmProjects\pythonProject1\chromedriver-win64\chromedriver.exe"
+
+import pytest
+class TestClass:
+
+    def test_1(self,setup):
+        print("I am,happy")
+
+    def test_2(self,setup):
+
+        print("i, Am good")
