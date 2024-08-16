4cb2450e248ea4769dfbb768452ae6f4ffd32d9c
witvick <nitishkumar37373@gmail.com>
2024-08-16 20:40:42 +0530

    Login pAge

---
+++ Test_Opencart/test_login.py
@@ -0,0 +1,26 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from Test_Opencart.LoginPageObjects import RegisterPage
+class Testlogin:
+    def test_login(self,setup):
+        # from selenium.webdriver.chrome.service import Service
+        # self.ser_obj = Service(r"C:\Users\MR.WICK\PycharmProjects\pythonProject1\chromedriver-win64\chromedriver.exe")
+        # self.driver = webdriver.Chrome(service=self.ser_obj)
+        self.driver = setup
+        time.sleep(6)
+        self.driver.get("https://practice.automationtesting.in/my-account/")
+        self.driver.maximize_window()
+
+        self.reg = RegisterPage(self.driver)
+        self.reg.loginEmail("abcdef14@gmail.com")
+        self.reg.loginPassword("ABCD123KSIL")
+        self.reg.login()
+        time.sleep(10)
+        self.actual_title = self.driver.title
+        self.driver.close()
+        assert self.actual_title == "My Account – Automation Practice Site"
+
