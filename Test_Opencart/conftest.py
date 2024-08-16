af8a8270d73353874da82598b2af75310b0bc732
witvick <nitishkumar37373@gmail.com>
2024-08-16 11:09:49 +0530

    parallel browser CLI

--- Test_Opencart/conftest.py
+++ Test_Opencart/conftest.py
@@ -1,10 +1,37 @@
 import pytest
-
+from selenium.webdriver import chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture()
-def setup():
-    print("Launching Browser")
-    yield
-    print("Closing Browser")
+def setup(browser):
+
+    if browser=="chrome":
+        from selenium.webdriver.chrome.service import Service
+        ser_obj = Service(r"C:\Users\MR.WICK\PycharmProjects\pythonProject1\chromedriver-win64\chromedriver.exe")
+        driver = webdriver.Chrome(service=ser_obj)
+
+
+    elif browser == "edge":
+        from selenium.webdriver.edge.service import Service
+        ser_obj = Service(r"C:\Users\MR.WICK\PycharmProjects\pythonProject1\edgedriver_win64\msedgedriver.exe")
+        driver = webdriver.Edge(service=ser_obj)
+
+
+    elif browser == "firefox":
+        from selenium.webdriver.firefox.service import Service
+        ser_obj = Service(r"C:\Users\MR.WICK\PycharmProjects\pythonProject1\geckodriver-v0.34.0-win64\geckodriver.exe")
+        driver = webdriver.Firefox(service=ser_obj)
+
+
+    return driver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", help="my option: type1 or type2")
+
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")
 
 
